5f0d712366c114334c5666ad18da9ebc959c3d68
AzeemAleem <azeem.attari41@gmail.com>
2020-10-19 00:37:45 +0500

    Updates

---
+++ precache-manifest.26a2a49626186917081ffa023c4ead41.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54260b92f1895561be5ad3e2dd92b620",
+    "url": "/ghoraApp/index.html"
+  },
+  {
+    "revision": "2bf2516578489aaa3445",
+    "url": "/ghoraApp/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "f26a8b3dd3325108fbe8",
+    "url": "/ghoraApp/static/css/main.ab6b3ca3.chunk.css"
+  },
+  {
+    "revision": "2bf2516578489aaa3445",
+    "url": "/ghoraApp/static/js/2.e59e8cd1.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/ghoraApp/static/js/2.e59e8cd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f26a8b3dd3325108fbe8",
+    "url": "/ghoraApp/static/js/main.b0cbbdda.chunk.js"
+  },
+  {
+    "revision": "60ed46e1bfd9dae55643",
+    "url": "/ghoraApp/static/js/runtime-main.f135a943.js"
+  },
+  {
+    "revision": "5dca90bbb083d704c94200d6aaa5ed3c",
+    "url": "/ghoraApp/static/media/c1-01.5dca90bb.jpg"
+  },
+  {
+    "revision": "55e9ca972dfdb07f09db7e04d82c5766",
+    "url": "/ghoraApp/static/media/c1-02.55e9ca97.jpg"
+  },
+  {
+    "revision": "bd327c27cd558113f471504c047a60c9",
+    "url": "/ghoraApp/static/media/c1-03.bd327c27.jpg"
+  },
+  {
+    "revision": "7b1a861af1012087cd70908fc88827a5",
+    "url": "/ghoraApp/static/media/d1-01.7b1a861a.jpg"
+  },
+  {
+    "revision": "82d64a18d7624fc29d4ebd24c97b17b4",
+    "url": "/ghoraApp/static/media/d1-02.82d64a18.jpg"
+  },
+  {
+    "revision": "bacf64e227ae0b54b92e086e7c0aad4e",
+    "url": "/ghoraApp/static/media/d1-03.bacf64e2.jpg"
+  },
+  {
+    "revision": "673c5ec0803b4562e2e10bd9da99a414",
+    "url": "/ghoraApp/static/media/ghora.673c5ec0.png"
+  },
+  {
+    "revision": "7173769d876f1bb393d794563f62b4af",
+    "url": "/ghoraApp/static/media/h1-01.7173769d.jpg"
+  },
+  {
+    "revision": "0d460d032afb3174a99d1e20997a668e",
+    "url": "/ghoraApp/static/media/h1-02.0d460d03.jpg"
+  },
+  {
+    "revision": "fe7ceacd69bec99b1b49fe42c750dbfe",
+    "url": "/ghoraApp/static/media/h1-03.fe7ceacd.jpg"
+  },
+  {
+    "revision": "fdfdb09d8e3f7ae8e86a9d08bb099d88",
+    "url": "/ghoraApp/static/media/horse-running.fdfdb09d.jpg"
+  },
+  {
+    "revision": "ab7520ffbf014b4176732793dfa03fa9",
+    "url": "/ghoraApp/static/media/logo.ab7520ff.png"
+  },
+  {
+    "revision": "7c85b6f6379ed030aaeb2c8c8e152046",
+    "url": "/ghoraApp/static/media/o1-01.7c85b6f6.jpg"
+  },
+  {
+    "revision": "93e1a7cd5732e8412388f579aa670c0c",
+    "url": "/ghoraApp/static/media/o1-02.93e1a7cd.jpg"
+  },
+  {
+    "revision": "4603696695b70370789b2dba3e34fefd",
+    "url": "/ghoraApp/static/media/o1-03.46036966.jpg"
+  },
+  {
+    "revision": "8822d9e1acb2552414a90bd1be179c3b",
+    "url": "/ghoraApp/static/media/profile.8822d9e1.jpg"
+  }
+]);
